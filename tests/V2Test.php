d2696e251bc303c57670eafa28cc0d27ad2c10af
耗子 <i@haozi.net>
2023-03-03 19:41:18 +0800

    Update V2 tests

---
+++ tests/V2Test.php
@@ -0,0 +1,18 @@
+<?php
+
+use HaoziTeam\ChatGPT\V2;
+
+$secretKey = getenv('OPENAI_SECRET_KEY');
+$chatGPT = new V2($secretKey);
+$chatGPT->addMessage('You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.', 'system');
+
+it('should get a new conversation', function () use ($chatGPT) {
+    $return = $chatGPT->ask('Hello, how are you?');
+    $this->assertArrayHasKey('answer', $return);
+})->group('working');
+
+it('should get a answer contact the context', function () use ($chatGPT) {
+    $return = $chatGPT->ask('What did I ask you just now?');
+    $this->assertArrayHasKey('answer', $return);
+    $this->assertStringContainsString('Hello, how are you?', $return['answer']);
+})->group('working');
